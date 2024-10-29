bd57f4516eccbece6481f3ae063b63853936d50d
yyu26 <yyu26@slb.com>
2024-10-29 15:56:15 +0800

    add getPrimeFactors func

--- src/add.ts
+++ src/add.ts
@@ -1,3 +1,15 @@
-export function add(a: number, b: number) {
-  return a + b;
+export function getPrimeFactors(a: number): number[] {
+  let nums: number[] = [];
+  let i = 2;
+  while (i * i <= a) {
+    while (a % i === 0) {
+      nums.push(i);
+      a = a / i;
+    }
+    i++;
+  }
+  if (a > 1) {
+    nums.push(a);
+  }
+  return nums;
 }
--- test/add.test.ts
+++ test/add.test.ts
@@ -1,7 +1,31 @@
-import { add } from "../src/add";
+import { getPrimeFactors } from "../src/add";
 
-describe("add", () => {
-  it("should add two numbers", () => {
-    expect(add(1, 2)).toBe(3);
+describe("getPrimeFactors", () => {
+  it("should get PrimeFactors[2,2] when input 4", () => {
+    expect(getPrimeFactors(4)).toEqual([2, 2]);
+  });
+});
+
+describe("getPrimeFactors", () => {
+  it("should get PrimeFactors[2,3] when input 6", () => {
+    expect(getPrimeFactors(6)).toEqual([2, 3]);
+  });
+});
+
+describe("getPrimeFactors", () => {
+  it("should get PrimeFactors[2,2,5] when input 20", () => {
+    expect(getPrimeFactors(20)).toEqual([2, 2, 5]);
+  });
+});
+
+describe("getPrimeFactors", () => {
+  it("should get PrimeFactors[] when input 1", () => {
+    expect(getPrimeFactors(1)).toEqual([]);
+  });
+});
+
+describe("getPrimeFactors", () => {
+  it("should get PrimeFactors[83] when input 83", () => {
+    expect(getPrimeFactors(83)).toEqual([83]);
   });
 });
