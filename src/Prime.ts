711dfa001f66f6ef739286540ca5901f599b18a7
ZhengdanWang <zwang@slb.com>
2024-10-29 15:20:23 +0800

    fix: fixing an infinite loop bug

--- src/Prime.ts
+++ src/Prime.ts
@@ -5,11 +5,17 @@ export function getPrimeFactors(value: number): number[] {
   ];
 
   while (value != 1) {
+    let flag = false;
     for (let i = 0; i < primes.length; i++) {
       if (value % primes[i] == 0) {
         re.push(primes[i]);
         value /= primes[i];
+        flag = true;
       }
+    }
+    if (flag == false) {
+      re.push(value);
+      break;
     }
   }
   re.sort();
