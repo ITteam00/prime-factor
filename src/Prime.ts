a4454f4f0d462b3bd844ec1bb38e566088299798
ZhengdanWang <zwang@slb.com>
2024-10-29 16:02:23 +0800

    tde: 4

--- src/Prime.ts
+++ src/Prime.ts
@@ -1,7 +1,8 @@
 export function getPrimeFactors(value: number): number[] {
   const re: number[] = [];
-  if (value % 2 == 0) {
+  while (value % 2 == 0) {
     re.push(2);
+    value /= 2;
   }
 
   return re;
--- test/add.test.ts
+++ test/add.test.ts
@@ -4,6 +4,7 @@ describe("getPrimeFactors", () => {
   it("should calculate prime factors", () => {
     expect(getPrimeFactors(1)).toEqual([]);
     expect(getPrimeFactors(2)).toEqual([2]);
+    expect(getPrimeFactors(4)).toEqual([2, 2]);
 
 
   });
